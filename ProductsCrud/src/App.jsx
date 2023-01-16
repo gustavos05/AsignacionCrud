1c4db20a71b6050b86f07d0e3fe4145d2ecc49ae
Gustavo <ingsuarezgustavo@gmail.com>
2023-01-16 19:49:32 -0400

    first commit

---
+++ ProductsCrud/src/App.jsx
@@ -0,0 +1,69 @@
+import "./index.css";
+import { useState, useEffect } from "react";
+import FormProduct from "./Components/FormProduct";
+import ProductCard from "./Components/ProductCard";
+import axios from "axios";
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [productDataUpdate, setProductDataUpdate] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://products-crud.academlo.tech/products/")
+      .then((resp) => setProducts(resp.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const getAPIData = () => {
+    axios
+      .get("https://products-crud.academlo.tech/products/")
+      .then((resp) => setProducts(resp.data))
+      .catch((error) => console.error(error));
+  };
+
+  const addProducts = (data) => {
+   
+    axios
+      .post("https://products-crud.academlo.tech/products/", data)
+      .then(() => getAPIData())
+      .catch((error) => console.error(error));
+  };
+
+  const deleteProduct = (productId) => {
+  
+    axios 
+      .delete(`https://products-crud.academlo.tech/products/${productId}/`)
+      .then(() => getAPIData())
+      .catch((error) => console.error(error));
+  };
+
+  const selectProduct = (productData) => {
+    setProductDataUpdate(productData);
+  };
+
+  const updateProduct = (editedProduct) => {
+    const index = products.findIndex((product) => product.id === editedProduct.id);
+
+    products[index] = editedProduct;
+    setProducts([...products]);
+    setProductDataUpdate(null);
+  };
+
+  return (
+    <div className="App">
+      <FormProduct
+        createProductData={(data) => addProducts(data)}
+        productSelectedData={productDataUpdate}
+        updateProduct={updateProduct}
+      />
+      <ProductCard
+        products={products}
+        deleteProduct={deleteProduct}
+        selectProduct={selectProduct}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ ProductsCrud/src/Components/FormProduct.jsx
@@ -0,0 +1,102 @@
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+
+const FormProduct = ({ createProductData, productSelectedData, updateProduct }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm();
+  
+
+  const getFormData = (data) => {
+    
+    console.log(data);
+    if (productSelectedData) {
+      
+      updateProduct(data);
+    } else {
+
+      createProductData(data);
+
+      resetForm();
+    }
+  };
+
+  useEffect(() => {
+    if (productSelectedData !== null) {
+      //Hay elemento seleccionado
+      reset(productSelectedData);
+    } else {
+      //No hay elemento seleccionado
+      resetForm();
+    }
+  }, [productSelectedData]);
+
+  const resetForm = () => {
+    reset({
+      first_name: "",
+      email: "",
+      last_name: "",
+      password: "",
+      birthday: ""
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(getFormData)}>
+        <div className="input-wrapper">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            {...register("name", {
+              required: true
+            })}
+          />
+          {errors.name?.type === "required" && (
+            <span role="alert">Este input es requerido</span>
+          )}
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="category">Category</label>
+          <input
+            type="text"
+            id="category"
+            {...register("category", {
+              required: true
+            })}
+          />
+          {errors.name?.type === "required" && (
+            <span role="alert">Este input es requerido</span>
+          )}
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="Product-Price">Price</label>
+          <input type='number' id="price" {...register("price")} />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="isAvailable">Available</label>
+          <input
+            type='checkbox'
+            id="isAvailable"
+            {...register("isAvailable", {
+              required: true
+            })}
+          />
+          {errors.name?.type === "required" && (
+            <span role="alert">Este input es requerido</span>
+          )}
+        </div>
+
+        <button className="btn-edit" type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FormProduct;
