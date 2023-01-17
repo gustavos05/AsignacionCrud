36abff6144bcd06fc3101b33298d0ee8c4a0b2ba
Gustavo <ingsuarezgustavo@gmail.com>
2023-01-17 10:18:24 -0400

    add2

--- ProductsCrud/src/Components/ProductCard.jsx
+++ ProductsCrud/src/Components/ProductCard.jsx
@@ -7,13 +7,12 @@ const ProductCard = ({products, deleteProduct, selectProduct}) => {
         <div className="Wrapper">
             {
                 products?.map( (productElement, index) => (
-                    <div className="Card" key={`product-${index}`}>
+                    <div className="Card" key={`products-${index}`}>
                         <h4><span>Name:</span> {productElement.name}</h4>
                         <h4><span>Category: </span> {productElement.category}</h4>
                         <button className="btn-delete" onClick={ () => deleteProduct(productElement.id) }>Delete</button>
                         <button className="btn-edit" onClick={ () => selectProduct(productElement) } >Select</button>
-                       
-                        </div>
+                    </div>
                 ) )
             }
        </div>
